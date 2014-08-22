787c476456ecd1611fa9e0dd58e1382fe3fe9387
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2014-08-22 01:22:25 -0500

    First advance in Python Threads for SAT Problem
    Trying to improve python SAT problem using threads; the performance could be checked using in a unix terminal : time python fillingArrayNoThreads.py 20; including with threads python file. It's necessary to have numpy python libraries; the librarie itself improves python arrays ;)

---
+++ test/pythonThreads/fillingArrayThreads.py
@@ -0,0 +1,38 @@
+import sys
+import numpy as np
+import thread
+import time
+
+variables = 4
+
+if len(sys.argv) is not 1:
+    variables = int(sys.argv[1])
+numberposibilites = 2 ** variables
+array = np.ones((numberposibilites,variables+2))
+#threads
+def fillArray( threadName, startPoint, endPoint ):
+    print "From thread [ "+threadName + " ]\n"
+    for column in range(variables, 0, -1):
+        for row in range(startPoint, endPoint):
+            if row % (2 ** column) < 2 ** (column - 1):
+                array[row, column - 1] = 1
+            else:
+                array[row, column - 1] = 0
+
+try:
+    if variables >= 4:
+        thread.start_new_thread( fillArray, ("Thread-1", 0, numberposibilites/4 ) )
+        thread.start_new_thread( fillArray, ("Thread-2", (numberposibilites/4), (numberposibilites/4)*2 ) )
+        thread.start_new_thread( fillArray, ("Thread-3", (numberposibilites/4)*2, (numberposibilites/4)*3 ) )
+        thread.start_new_thread( fillArray, ("Thread-4", (numberposibilites/4)*3, numberposibilites ) )
+    else:
+        fillArray("Main", 0,)
+except Exception, e:
+    raise
+else:
+    pass
+finally:
+    pass
+    #for row in range(len(array)):
+    #    print array[row]
+    #    print "\n"
